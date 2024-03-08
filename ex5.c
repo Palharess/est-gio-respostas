d627c9dd0cf8d79705cacaed07077379ad6ce156
Gabriel Palhares <123818597+Palharess@users.noreply.github.com>
2024-03-08 16:55:52 -0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -7,7 +7,7 @@ int main(){
     char frase[100];
     char temp;
     scanf("%[^\n]", frase);
-    int tamanho = strlen(frase); // função pronta apenas para pegar o tamanho, como poderia predeterminar uma tamanho não vi problema em utilizar
+    int tamanho = strlen(frase); // função pronta apenas para pegar o tamanho, como poderia predeterminar um tamanho não vi problema em utilizar
     for(int i = 0; i < tamanho/2; i++){
         temp = frase[i];
         frase[i] = frase[tamanho - 1 - i];
